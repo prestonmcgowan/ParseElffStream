eeebde97ef5c067bbba49039ad32d6f2217a997c
Yemalin Godonou <ygodonou@confluent.io>
2021-03-25 17:18:32 -0500

    adding a testing for output in kafka topics

--- src/main/java/io/confluent/ps/convert/ParseElffStream.java
+++ src/main/java/io/confluent/ps/convert/ParseElffStream.java
@@ -9,10 +9,10 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.github.jcustenborder.parsers.elf.ElfParser;
 import com.github.jcustenborder.parsers.elf.ElfParserBuilder;
@@ -20,16 +20,15 @@ import com.github.jcustenborder.parsers.elf.LogEntry;
 
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
+import org.glassfish.jersey.internal.util.Producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -57,11 +56,11 @@ public final class ParseElffStream {
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, envProps.getProperty("bootstrap.servers"));
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, String().getClass());
-        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, envProps.getProperty("security.protocol"));
+        /**props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, envProps.getProperty("security.protocol"));
         props.put(StreamsConfig.SECURITY_PROTOCOL_CONFIG, envProps.getProperty("security.protocol"));
         props.put(SaslConfigs.SASL_MECHANISM, envProps.getProperty("sasl.mechanism"));
         props.put(SaslConfigs.SASL_JAAS_CONFIG, envProps.getProperty("sasl.jaas.config"));
-
+**/
         log.debug("SASL Config------");
         log.debug("bootstrap.servers={}", envProps.getProperty("bootstrap.servers"));
         log.debug("security.protocol={}", envProps.getProperty("security.protocol"));
@@ -93,9 +92,14 @@ public final class ParseElffStream {
         // topic contains byte data
         final KStream<Integer, Bytes> elffStream =
             builder.stream(inputTopicName, Consumed.with(Serdes.Integer(), Serdes.Bytes()));
+        ;
 
         // decompress gzip data into a string
-        elffStream.mapValues( elffData -> {
+
+        elffStream.mapValues( elffData-> {
+            //System.out.println(elffData.toString().replace("\\x0A", "\\r\\n"));
+            //System.out.println("------------------Done printing elffString --------------------------");
+
             List<LogEntry> messages = new ArrayList<>();
 
             // Grab the ELFF String or compressed ELFF Message
@@ -118,33 +122,49 @@ public final class ParseElffStream {
 
             // Now that we have the ELFF String, parse it
             try {
-                Reader targetReader = new StringReader(elffString);
-                ElfParser parser = ElfParserBuilder.of()
-                 .build(targetReader);
-
+                String elffreplace = elffString.replace("\\x0A", "\n");
+                Reader targetReader = new StringReader(elffreplace);
+                System.out.println("------------------processing elffString --------------------------");
+                System.out.println(elffreplace);
+                System.out.print(targetReader);
+                ElfParser parser = ElfParserBuilder.of().build(targetReader);
+                System.out.println("------------------Done processing elffString --------------------------");
                  // Copy the messages to our output
                 LogEntry entry;
                 while (null != (entry = parser.next())) {
                     messages.add(entry);
                 }
+
             } catch (IOException e) {
                 log.info("Parsing Exception");
                 e.printStackTrace();
             }
 
+            System.out.println(messages);
+
             return messages;
-        })
-        .to(outputTopicName); // , Produced.valueSerde(Serdes.String()));
+
+        }).map((key, value) -> {
+            return
+            KeyValue.pair(
+                    UUID.randomUUID().toString(),
+                    value.toString()
+            );
+                }
+        )
+        .to(outputTopicName);//to(outputTopicName, Produced<Integer, LogEntry>); // , Produced.valueSerde(Serdes.String()));
+
         //TODO: Implement this so it works, it should be before the .to(...)
         // .mapValues( message -> {
         //     log.info(message);
         //     //TODO: Push into JSON using gson library
         //     //TODO: each message individually to the output topic (one to many)
         // })
-
+        //elffStream.to(outputTopicName);
 
         return builder.build();
       }
+
 
       /**
        * Load in the Environment Properties that were passed in from the CLI.
